2f19ff32916d2f6b63f2f3cacc479b065c165b5c
unknown <isc.juan.gallard@gmail.com>
2024-05-27 13:43:23 -0600

    correccion

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import {Html5Qrcode} from "html5-qrcode";
 async function onScanSuccess(qrMessage, decodeResult) {
    
             try {
-                const response = await fetch(`http://localhost:8000/api/entradas/${qrMessage}`, {
+                const response = await fetch(`http://oscar.promocionesgallardo.com/api/entradas/${qrMessage}`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json'
